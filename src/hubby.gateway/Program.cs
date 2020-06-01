f0ae9634a660906d860b8edbfd8627d6121f5e08
SondreB <sondre@outlook.com>
2020-06-01 18:09:31 +0200

    Basic client/server setup

--- src/hubby.gateway/Program.cs
+++ src/hubby.gateway/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+using System;
+using System.Threading;
+using LiteNetLib;
+using LiteNetLib.Utils;
 
 namespace hubby.gateway
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         EventBasedNetListener listener = new EventBasedNetListener();
+         NetManager server = new NetManager(listener);
+         server.Start(15050);
+
+         listener.ConnectionRequestEvent += request =>
+         {
+            if (server.ConnectedPeersCount < 10)
+               request.AcceptIfKey("SomeConnectionKey");
+            else
+               request.Reject();
+         };
+
+         listener.PeerConnectedEvent += peer =>
+         {
+            Console.WriteLine("We got connection: {0}", peer.EndPoint); // Show peer ip
+
+            NetDataWriter writer = new NetDataWriter();                 // Create writer class
+            writer.Put("Hello client!");                                // Put some string
+            peer.Send(writer, DeliveryMethod.ReliableOrdered);             // Send with reliability
+         };
+
+         while (!Console.KeyAvailable)
+         {
+            server.PollEvents();
+            Thread.Sleep(15);
+         }
+         server.Stop();
+      }
+   }
 }
--- src/hubby/Program.cs
+++ src/hubby/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+using System;
+using System.Threading;
+using LiteNetLib;
 
 namespace hubby
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         EventBasedNetListener listener = new EventBasedNetListener();
+         NetManager client = new NetManager(listener);
+
+         client.Start();
+         client.Connect("city.hub.blockcore.net", 15050, "SomeConnectionKey");
+
+         listener.NetworkReceiveEvent += (fromPeer, dataReader, deliveryMethod) =>
+         {
+            Console.WriteLine("We got: {0}", dataReader.GetString(100 /* max length of string */));
+            dataReader.Recycle();
+         };
+
+         while (!Console.KeyAvailable)
+         {
+            client.PollEvents();
+            Thread.Sleep(15);
+         }
+
+         client.Stop();
+      }
+   }
 }
