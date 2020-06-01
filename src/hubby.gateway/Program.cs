4f74edca6d5537686e063a1eda94072cf8d36bcd
SondreB <sondre@outlook.com>
2020-06-01 18:05:25 +0200

    Add basic hubby console app and gateway app

---
+++ src/hubby.gateway/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hubby.gateway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/hubby/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hubby
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
